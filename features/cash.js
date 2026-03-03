16ba77976f6bf45c32885d998d17e11cc7d862d1
hgggopfunnu-star <hgggopfunnu@gmail.com>
2026-03-03 17:16:07 +0530

    Update cash.js

--- features/cash.js
+++ features/cash.js
@@ -2,25 +2,26 @@ module.exports = {
     name: "cash",
     description: "Check your Rift coin balance",
 
-    async execute(interaction, context) {
-        const user = getUser(interaction.user.id, context);
+    async execute(interaction, { balances }) {
+        const user = getUser(interaction.user.id, balances);
         return interaction.reply(`💰 You have **${user.coins} Rift Coins**`);
     },
 
-    async executePrefix(message, args, context) {
-        const user = getUser(message.author.id, context);
+    async executePrefix(message, args, { balances }) {
+        const user = getUser(message.author.id, balances);
         return message.reply(`💰 You have **${user.coins} Rift Coins**`);
     }
 };
 
-function getUser(userId, { balances }) {
-    if (!balances.has(userId)) {
+function getUser(userId, balances) {
+    if (!balances.has(userId) || typeof balances.get(userId) === "number") {
         balances.set(userId, {
-            coins: 0,
+            coins: typeof balances.get(userId) === "number" ? balances.get(userId) : 0,
             inventory: {},
             xp: 0,
             level: 1
         });
     }
+
     return balances.get(userId);
 }
